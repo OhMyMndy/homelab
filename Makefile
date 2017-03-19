52f09e475049f44ee55ea2288f4611e8bac050db
Mandy Schoep <mandyschoep@gmail.com>
2017-03-19 19:41:19 +0100

    Initial homelab setup

--- Makefile
+++ Makefile
@@ -1,6 +1,5 @@
 up: stop
 	docker-compose up -d
-	./scripts/pihole_set_ip
 
 restart: stop up
 	
--- docker-files/pi-hole/pihole_set_ip.sh
+++
@@ -1,2 +0,0 @@
-export MY_IP=$(ip route get 8.8.8.8 | awk '/8.8.8.8/ {print $NF}')
-sed -E -i 's/(IPV4_ADDRESS=)(.*)/\1${MY_IP}/g' /etc/pihole/setupVars.conf
