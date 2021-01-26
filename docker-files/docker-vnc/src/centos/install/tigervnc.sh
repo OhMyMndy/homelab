d186444105822df612c47085432cc08467cf17c2
Mandy Schoep <mandyschoep@gmail.com>
2021-01-26 14:44:41 +0100

    Remove docker-vnc

--- docker-files/docker-vnc/src/centos/install/tigervnc.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Install TigerVNC server"
-yum -y install tigervnc-server && yum clean all
--- docker-files/pi-hole/scripts/pihole_set_ip.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-
-export MY_IP=$(ip route get 8.8.8.8 | awk '/8.8.8.8/ {print $NF}')
-
-docker exec -it pihole bash -c "sed -E -i 's/(IPV4_ADDRESS=)(.*)/\1${MY_IP}/g' /etc/pihole/setupVars.conf"
