14703e0d0c52bc414538568e1182c6f0c3cb8a7a
Mandy Schoep <mandyschoep@gmail.com>
2024-03-27 18:06:10 +0100

    Portainer

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -23,6 +23,9 @@ TAILSCALE_IP="$(ip -f inet addr show tailscale0 | sed -En -e 's/.*inet ([0-9.]+)
 
 (cd adguard && docker-compose up -d --remove-orphans)
 
+(cd portainer && docker-compose up -d --remove-orphans)
+
+
 ( cd authentik;
 if [[ ! -f .env ]]; then
     echo "PG_PASS=$(openssl rand -base64 36)" >> .env;
