9600bcdfe334f1eb7c187640b6e67f907c7aad69
Mandy Schoep <mandyschoep@gmail.com>
2024-03-27 17:22:52 +0100

    Add adguard to deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -9,17 +9,19 @@ export TAILSCALE_IP
 
 TAILSCALE_IP="$(ip -f inet addr show tailscale0 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p')"
 
-(cd reverse-proxy && docker-compose up -d)
+(cd reverse-proxy && docker-compose up -d --remove-orphans)
 
-(cd tt-rss && docker-compose up -d)
+(cd tt-rss && docker-compose up -d --remove-orphans)
 
-(cd vaultwarden && docker-compose up -d)
+(cd vaultwarden && docker-compose up -d --remove-orphans)
 
 # (cd minecraft-bedrock-server && docker-compose up -d)
 
-(cd firefly && docker-compose up -d)
+(cd firefly && docker-compose up -d --remove-orphans)
 
-(cd dashy && docker-compose up -d) 
+(cd dashy && docker-compose up -d --remove-orphans) 
+
+(cd adguard && docker-compose up -d --remove-orphans)
 
 ( cd authentik;
 if [[ ! -f .env ]]; then
