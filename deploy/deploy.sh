3b36b65d6860394d64df61cf10cc1302e4dce527
Mandy Schoep <mandyschoep@gmail.com>
2024-03-27 17:20:05 +0100

    Adguard ai blocklist and bind to tailscale interface

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,6 +5,9 @@ DIR="$(dirname "$(readlink -f "$0")")/../"
 
 cd "$DIR"
 
+export TAILSCALE_IP
+
+TAILSCALE_IP="$(ip -f inet addr show tailscale0 | sed -En -e 's/.*inet ([0-9.]+).*/\1/p')"
 
 (cd reverse-proxy && docker-compose up -d)
 
