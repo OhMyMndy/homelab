a937f1a78b85b3b76c0c2f4dc603b4370b2564e3
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2024-12-16 14:17:45 +0100

    feat: openwebui

---
+++ openwebui/storage/pipelines/pipeline-add-user.py
@@ -0,0 +1,69 @@
+"""
+title: Chat Info Filter Pipeline
+author: changchiyou
+date: 2024-08-02
+version: 1.0
+license: MIT
+description: A filter pipeline that preprocess form data before requesting chat completion with LiteLLM.
+requirements:
+"""
+
+from typing import List, Optional
+from pydantic import BaseModel
+
+
+class Pipeline:
+    class Valves(BaseModel):
+        # List target pipeline ids (models) that this filter will be connected to.
+        # If you want to connect this filter to all pipelines, you can set pipelines to ["*"]
+        # e.g. ["llama3:latest", "gpt-3.5-turbo"]
+        pipelines: List[str] = []
+
+        # Assign a priority level to the filter pipeline.
+        # The priority level determines the order in which the filter pipelines are executed.
+        # The lower the number, the higher the priority.
+        priority: int = 0
+
+    def __init__(self):
+        # Pipeline filters are only compatible with Open WebUI
+        # You can think of filter pipeline as a middleware that can be used to edit the form data before it is sent to the OpenAI API.
+        self.type = "filter"
+
+        # Optionally, you can set the id and name of the pipeline.
+        # Best practice is to not specify the id so that it can be automatically inferred from the filename, so that users can install multiple versions of the same pipeline.
+        # The identifier must be unique across all pipelines.
+        # The identifier must be an alphanumeric string that can include underscores or hyphens. It cannot contain spaces, special characters, slashes, or backslashes.
+        self.name = "Chat Info Filter"
+
+        # Initialize
+        self.valves = self.Valves(
+            **{
+                "pipelines": ["*"],  # Connect to all pipelines
+                "priority": 0
+            }
+        )
+
+        self.chat_generations = {}
+        pass
+
+    async def on_startup(self):
+        # This function is called when the server is started.
+        print(f"on_startup:{__name__}")
+        pass
+
+    async def on_shutdown(self):
+        # This function is called when the server is stopped.
+        print(f"on_shutdown:{__name__}")
+        pass
+
+    async def on_valves_updated(self):
+        # This function is called when the valves are updated.
+        pass
+
+    async def inlet(self, body: dict, user: Optional[dict] = None) -> dict:
+        print(f"inlet:{__name__}")
+
+        if user and not body.get("user"):
+            body["user"] = user['email']
+
+        return body
