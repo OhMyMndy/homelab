ed612268ad09473c9334c2ed9110ff5600a7d75f
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2024-12-27 16:30:04 +0100

    feat: update deploy scripts

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -12,10 +12,11 @@ source deploy/functions.sh
 (source .env-cloudflare && export CF_API_EMAIL CF_API_KEY &&
     nix run nixpkgs#flarectl -- dns create-or-update --zone ohmymndy.com --name '*.home' --content $LOCAL_IP --type A) || true
 
-start reverse-proxy adguard smtprelay status vaultwarden dashy homepage wallabag \
+start reverse-proxy lldap adguard smtprelay status vaultwarden authentik dashy homepage wallabag \
     redmine tt-rss humblebundle-downloader searxng \
+    gitea-mirror \
     kiwix pokeblahaj \
-    litellm openwebui
+    litellm openwebui n8n
 
 (source .env-cloudflare && export CF_API_EMAIL CF_API_KEY &&
     nix run nixpkgs#flarectl -- dns list --zone ohmymndy.com)
--- deploy/functions.sh
+++ deploy/functions.sh
@@ -9,13 +9,23 @@ LOCAL_IP="$(ip addr show ens18 | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[
 export PUID
 export PGID
 
-export AUTHENTIK_TAG=2
-
 export TIMEZONE
 
 PUID="$(id -u)"
 PGID="$(id -g)"
 TIMEZONE=Europe/Brussels
+
+if ! command -v jq &>/dev/null; then
+  echo "jq not found..." 1>&2
+fi
+
+if ! command -v yq &>/dev/null; then
+  echo "yq not found..." 1>&2
+fi
+
+if ! command -v openssl &>/dev/null; then
+  echo "openssl not found..." 1>&2
+fi
 
 function start() {
   local stacks="$*"
@@ -25,4 +35,20 @@ function start() {
 
     (cd "$stack" && docker compose up -d --remove-orphans --build)
   done
+}
+
+function add_to_env() {
+  local key="$1"
+  local value="$2"
+  if ! grep -q "${key}=" .env; then
+    echo "${key}=${value}" >>.env
+  fi
+}
+
+function random_password() {
+  openssl rand -base64 38 | tr -d '\n'
+}
+
+function create_host_dirs() {
+  docker compose config | yq '.services[].volumes[].source' | grep "^$PWD" | tr '\n' '\0' | xargs -0 -r -I{} bash -c "mkdir -p {}"
 }
