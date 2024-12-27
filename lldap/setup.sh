4e8d083af9de46fad3465d678058d2bf3dbbbebe
Mandy Schoep <2277717+OhMyMndy@users.noreply.github.com>
2024-12-27 16:29:08 +0100

    feat: lldap

---
+++ lldap/setup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$DIR" || exit 1
+
+source ../deploy/functions.sh
+
+if [[ ! -f .env ]]; then
+  cp env-example .env
+fi
+
+add_to_env LLDAP_JWT_SECRET "$(random_password)"
+add_to_env LLDAP_KEY_SEED "$(random_password)$(random_password)"
+add_to_env LLDAP_LDAP_USER_PASS "$(random_password)"
+
+create_host_dirs
